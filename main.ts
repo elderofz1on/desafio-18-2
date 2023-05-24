21509ba46de336b2c3d747318489933e64f2ba81
Wagner <wprodriguesbr@gmail.com>
2023-05-24 17:06:20 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+// azul
+pins.onPulsed(DigitalPin.P14, PulseValue.High, function () {
+	
+})
+// verde
+pins.onPulsed(DigitalPin.P13, PulseValue.High, function () {
+	
+})
+// vermelho
+pins.onPulsed(DigitalPin.P12, PulseValue.High, function () {
+	
+})
+basic.forever(function () {
+	
+})
